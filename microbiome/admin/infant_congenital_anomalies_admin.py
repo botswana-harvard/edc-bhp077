10f575de672fff49cf1b9c49390a11338003e411
Leslie <leslie.kep@gmail.com>
2015-08-25 16:49:18 +0200

    admin site classes

---
+++ microbiome/admin/infant_congenital_anomalies_admin.py
@@ -0,0 +1,95 @@
+from django.contrib import admin
+
+from ..models import (
+    InfantCongenitalAnomalies, InfantCnsAbnormalityItems, InfantFacialDefectItems,
+    InfantCleftDisorderItems, InfantMouthUpGastrointestinalItems, InfantCardiovascularDisorderItems,
+    InfantRespiratoryDefectItems, InfantLowerGastrointestinalItems, InfantMaleGenitalAnomalyItems,
+    InfantFemaleGenitalAnomalyItems, InfantRenalAnomalyItems, InfantMusculoskeletalAbnormalItems,
+    InfantSkinAbnormalItems, InfantTrisomiesChromosomeItems
+)
+
+
+@admin.register(InfantCongenitalAnomalies)
+class InfantCongenitalAnomalies(admin.ModelAdmin):
+
+    list_display = ('infant_visit')
+
+
+@admin.register(InfantCnsAbnormalityItems)
+class InfantCnsAbnormalityItemsAdmin(admin.ModelAdmin):
+
+    list_display = ('congenital_anomalies', 'abnormality_status')
+
+    list_filter = ('cns_abnormality')
+
+
+@admin.register(InfantFacialDefectItems)
+class InfantFacialDefectItemsAdmin(admin.ModelAdmin):
+
+    list_display = ('congenital_anomalies')
+
+
+@admin.register(InfantCleftDisorderItems)
+class InfantCleftDisorderItemsAdmin(admin.ModelAdmin):
+
+    list_display = ('congenital_anomalies')
+
+
+@admin.register(InfantMouthUpGastrointestinalItems)
+class InfantMouthUpGastrointestinalItemsAdmin(admin.ModelAdmin):
+
+    list_display = ('congenital_anomalies')
+
+
+@admin.register(InfantCardiovascularDisorderItems)
+class InfantCardiovascularDisorderItemsAdmin(admin.ModelAdmin):
+
+    list_display = ('congenital_anomalies')
+
+
+@admin.register(InfantRespiratoryDefectItems)
+class InfantRespiratoryDefectItemsAdmin(admin.ModelAdmin):
+
+    list_display = ('congenital_anomalies')
+
+
+@admin.register(InfantLowerGastrointestinalItems)
+class InfantLowerGastrointestinalItemsAdmin(admin.ModelAdmin):
+
+    list_display = ('congenital_anomalies')
+
+
+@admin.register(InfantFemaleGenitalAnomalyItems)
+class InfantFemaleGenitalAnomalyItemsAdmin(admin.ModelAdmin):
+
+    list_display = ('congenital_anomalies')
+
+
+@admin.register(InfantMaleGenitalAnomalyItems)
+class InfantMaleGenitalAnomalyItemsAdmin(admin.ModelAdmin):
+
+    list_display = ('congenital_anomalies')
+
+
+@admin.register(InfantRenalAnomalyItems)
+class InfantRenalAnomalyItemsAdmin(admin.ModelAdmin):
+
+    list_display = ('congenital_anomalies')
+
+
+@admin.register(InfantMusculoskeletalAbnormalItems)
+class InfantMusculoskeletalAbnormalItemsAdmin(admin.ModelAdmin):
+
+    list_display = ('congenital_anomalies')
+
+
+@admin.register(InfantSkinAbnormalItems)
+class InfantSkinAbnormalItemsAdmin(admin.ModelAdmin):
+
+    list_display = ('congenital_anomalies')
+
+
+@admin.register(InfantTrisomiesChromosomeItems)
+class InfantTrisomiesChromosomeItemsAdmin(admin.ModelAdmin):
+
+    list_display = ('congenital_anomalies')
---
+++ microbiome/admin/infant_death_admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from ..models import InfantDeath
+
+
+@admin.register(InfantDeath)
+class InfantDeathAdmin(admin.ModelAdmin):
+
+    list_display = ('study_drug_relate', 'infant_nvp_relate', 'haart_relate')
+
+    list_filter = ('study_drug_relate')
