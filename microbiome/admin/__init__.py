55eaa96ce593e5be152f160242e913e6a2a8cb15
Leslie <leslie.kep@gmail.com>
2015-08-25 16:58:46 +0200

    update admin imports

--- microbiome/admin/__init__.py
+++ microbiome/admin/__init__.py
@@ -5,3 +5,13 @@ from .infant_birth_arv_admin import InfantBirthArvAdmin
 from .infant_birth_exam_admin import InfantBirthExamAdmin
 from .infant_birth_feeding_admin import InfantBirthFeedVaccineAdmin
 from .infant_birth_admin import InfantBirthAdmin
+from .infant_congenital_anomalies_admin import (
+    InfantCongenitalAnomalies, InfantCnsAbnormalityItemsAdmin,
+    InfantFacialDefectItemsAdmin, InfantCleftDisorderItemsAdmin, InfantMouthUpGastrointestinalItemsAdmin,
+    InfantCardiovascularDisorderItemsAdmin, InfantRespiratoryDefectItemsAdmin, InfantLowerGastrointestinalItemsAdmin,
+    InfantFemaleGenitalAnomalyItemsAdmin, InfantMaleGenitalAnomalyItemsAdmin, InfantRenalAnomalyItemsAdmin,
+    InfantMusculoskeletalAbnormalItemsAdmin, InfantSkinAbnormalItemsAdmin, InfantTrisomiesChromosomeItemsAdmin)
+from .infant_death_admin import InfantDeathAdmin
+from .infant_eligibility_admin import InfantEligibilityAdmin
+from .infant_off_study_admin import InfantOffStudyAdmin
+from .infant_visit_admin import InfantVisitAdmin
