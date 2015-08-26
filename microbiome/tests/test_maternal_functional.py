bb4ffc6f28d345809bfe6cfec30c55c7366ab0c0
melissa kewagamang <mrbkew@gmail.com>
2015-08-27 00:51:25 +0200

    update maternal functional

--- microbiome/tests/test_maternal_functional.py
+++ microbiome/tests/test_maternal_functional.py
@@ -1,3 +1,4 @@
+import time
 from microbiome.tests.base_selenium_test import BaseSeleniumTest
 
 
@@ -7,6 +8,20 @@ class TestMaternalFunctional(BaseSeleniumTest):
         '''Login and click enroll mother button'''
         self.login()
         enroll_mother = self.browser.find_elements_by_css_selector("a[href*='/admin/microbiome/maternaleligibilitypre/add/?next=/dashboard/']")
-        for e in range(0, len(enroll_mother)):
+        for mother_link in range(0, len(enroll_mother)):
+            mother_link = self.browser.find_elements_by_css_selector("a[href*='/admin/microbiome/maternaleligibilitypre/add/?next=/dashboard/']")
+            mother_link[0].click()
+
+    def test_microbiome_link(self):
+        '''Confirm page heading and click microbiome link'''
+        self.login()
+        time.sleep(1)
+        enroll_mother = self.browser.find_elements_by_css_selector("a[href*='/admin/microbiome/maternaleligibilitypre/add/?next=/dashboard/']")
+        for mylink in range(0, len(enroll_mother)):
             mylink = self.browser.find_elements_by_css_selector("a[href*='/admin/microbiome/maternaleligibilitypre/add/?next=/dashboard/']")
             mylink[0].click()
+        time.sleep(1)
+        heading = self.browser.title.lower().startswith('Microbiome administration')
+        self.assertTrue('Microbiome administration', heading)
+        time.sleep(1)
+        self.browser.find_element_by_css_selector("a[href*='/admin/microbiome/']").click()
