288f3920fea9329893740157276105a80ee8d202
melissa kewagamang <mrbkew@gmail.com>
2015-08-26 14:56:57 +0200

    corrections, updates

--- microbiome/admin/__init__.py
+++ microbiome/admin/__init__.py
@@ -15,3 +15,11 @@ from .infant_visit_admin import InfantVisitAdmin
 from .maternal_eligibility_post_admin import MaternalEligibilityPost
 from .maternal_eligibility_pre_admin import MaternalEligibilityPreAdmin
 from .subject_consent_admin import SubjectConsentAdmin
+from .maternal_locator_admin import MaternalLocatorAdmin
+from .maternal_labour_del_admin import (MaternalLabourDelAdmin, MaternalLabDelMedAdmin,
+                                        MaternalLabDelClinicAdmin, MaternalLabDelDxAdmin, 
+                                        MaternalLabDelDxTAdmin)
+from .maternal_infected_admin import MaternalInfectedAdmin
+from .maternal_enroll_arv_admin import MaternalEnrollArvAdmin
+from .maternal_arv_preg_admin import MaternalArvPregAdmin
+from .maternal_enrollment_post_admin import MaternalEnrollmentPostAdmin
