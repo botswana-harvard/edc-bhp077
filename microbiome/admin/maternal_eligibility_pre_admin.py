b35e8ff16f3997f5ef9c15d0e00e76788090a959
ckgathi <ckgathi@bhp.org.bw>
2015-08-26 04:04:39 +0200

    make the link to enroll mother work

--- microbiome/admin/maternal_eligibility_pre_admin.py
+++ microbiome/admin/maternal_eligibility_pre_admin.py
@@ -5,4 +5,19 @@ from ..models import MaternalEligibilityPre
 
 @admin.register(MaternalEligibilityPre)
 class MaternalEligibilityPreAdmin(admin.ModelAdmin):
-    pass
+
+    fields = (
+        'report_datetime',
+        'first_name',
+        'initials',
+        'dob',
+        'gender',
+        'has_identity',
+        'citizen',
+        'disease',
+        'currently_pregnant',
+        'pregnancy_weeks',
+        'verbal_hiv_status',
+        'evidence_pos_hiv_status',
+        'rapid_test_result',
+    )
