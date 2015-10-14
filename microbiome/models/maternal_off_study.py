885acb52321d5c700b56ab51e64a691c4d499fc1
melissa kewagamang <mrbkew@gmail.com>
2015-10-14 08:50:06 +0200

    add EntryMetaDataManager to offstudy

--- microbiome/models/maternal_off_study.py
+++ microbiome/models/maternal_off_study.py
@@ -1,6 +1,11 @@
-from edc_base.audit_trail import AuditTrail
+from django.db import models
+
+from edc.entry_meta_data.managers import EntryMetaDataManager
 from edc.subject.off_study.models import BaseOffStudy
+from edc_base.audit_trail import AuditTrail
 from edc_base.model.models.base_uuid_model import BaseUuidModel
+
+from .maternal_visit import MaternalVisit
 
 
 class MaternalOffStudy(BaseOffStudy, BaseUuidModel):
@@ -8,6 +13,10 @@ class MaternalOffStudy(BaseOffStudy, BaseUuidModel):
     """A model completed by the user that completed when the subject is taken off-study."""
 
     history = AuditTrail()
+
+    maternal_visit = models.OneToOneField(MaternalVisit)
+
+    entry_meta_data_manager = EntryMetaDataManager(MaternalVisit)
 
     class Meta:
         app_label = "microbiome"
