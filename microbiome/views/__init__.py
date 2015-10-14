85fd40e43fa64adc23b9dcca8fd81c606e220821
melissa kewagamang <mrbkew@gmail.com>
2015-10-14 09:23:36 +0200

    remove old & unused teplates | views

--- microbiome/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from .home_view import HomeView
-from .login import login_view
-from .logout import logout_view
-from .subject_dashboard import SubjectDashboardView
-from .maternal_search_view import MaternalSearchView
