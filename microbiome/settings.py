01eebfe96fd16d1394bb011af0b21a7d38e3e367
melissa kewagamang <mrbkew@gmail.com>
2015-10-14 09:17:57 +0200

    add dajax setting

--- microbiome/settings.py
+++ microbiome/settings.py
@@ -180,6 +180,9 @@ DATABASES = {
     }
 }
 
+# dajax
+DAJAXICE_MEDIA_PREFIX = "dajaxice"
+
 DEVICE_ID = 95
 IS_SECURE_DEVICE = True
 FIELD_MAX_LENGTH = 'default'
@@ -200,7 +203,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
