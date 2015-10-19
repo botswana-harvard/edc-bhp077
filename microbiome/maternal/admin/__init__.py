9a655f4e256feb35f834c7108d41e3b5d002bc0e
Florence Chilisa <fchilisa@gmail.com>
2015-10-19 11:43:09 +0200

    fix import

--- microbiome/maternal/admin/__init__.py
+++ microbiome/maternal/admin/__init__.py
@@ -1,16 +1,16 @@
-from maternal.admin.maternal_eligibility_admin import MaternalEligibilityAdmin
-from maternal.admin.antenatal_enrollment_admin import AntenatalEnrollmentAdmin
-from maternal.admin.postnatal_enrollment_admin import PostnatalEnrollmentAdmin
-from maternal.admin.maternal_consent_admin import MaternalConsentAdmin
-from maternal.admin.sample_consent_admin import SampleConsentAdmin
-from maternal.admin.maternal_locator_admin import MaternalLocatorAdmin
-from maternal.admin.maternal_demographics_admin import MaternalDemographicsAdmin
-from maternal.admin.maternal_medical_history_admin import MaternalMedicalHistoryAdmin
-from maternal.admin.maternal_obsterical_history_admin import MaternalObstericalHistoryAdmin
-from maternal.admin.maternal_clinical_history_admin import MaternalClinicalHistoryAdmin
-from maternal.admin.maternal_labour_del_admin import (MaternalLabourDelAdmin, MaternalLabDelMedAdmin,
+from .maternal_eligibility_admin import MaternalEligibilityAdmin
+from .antenatal_enrollment_admin import AntenatalEnrollmentAdmin
+from .postnatal_enrollment_admin import PostnatalEnrollmentAdmin
+from .maternal_consent_admin import MaternalConsentAdmin
+from .sample_consent_admin import SampleConsentAdmin
+from .maternal_locator_admin import MaternalLocatorAdmin
+from .maternal_demographics_admin import MaternalDemographicsAdmin
+from .maternal_medical_history_admin import MaternalMedicalHistoryAdmin
+from .maternal_obsterical_history_admin import MaternalObstericalHistoryAdmin
+from .maternal_clinical_history_admin import MaternalClinicalHistoryAdmin
+from .maternal_labour_del_admin import (MaternalLabourDelAdmin, MaternalLabDelMedAdmin,
                                         MaternalLabDelClinicAdmin, MaternalLabDelDxAdmin,
                                         MaternalLabDelDxTAdmin)
-from maternal.admin.maternal_infected_admin import MaternalInfectedAdmin
-from maternal.admin.maternal_arv_history_admin import MaternalArvHistoryAdmin
-from maternal.admin.maternal_arv_preg_admin import MaternalArvPregAdmin
+from .maternal_infected_admin import MaternalInfectedAdmin
+from .maternal_arv_history_admin import MaternalArvHistoryAdmin
+from .maternal_arv_preg_admin import MaternalArvPregAdmin
