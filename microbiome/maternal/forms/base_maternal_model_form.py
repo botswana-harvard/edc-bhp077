01e9b7ab09972971a6d6c0be2cbe9e180a86d381
melissa kewagamang <mrbkew@gmail.com>
2015-10-21 09:50:11 +0200

    add base_maternal_model_form

---
+++ microbiome/maternal/forms/base_maternal_model_form.py
@@ -0,0 +1,14 @@
+from edc_consent.forms import BaseConsentedModelForm
+
+from ..models import MaternalVisit
+
+
+class BaseMaternalModelForm(BaseConsentedModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(BaseMaternalModelForm, self).__init__(*args, **kwargs)
+        try:
+            self.fields['maternal_visit'].queryset = MaternalVisit.objects.filter(
+                pk=self.instance.maternal_visit.pk)
+        except:
+            pass
