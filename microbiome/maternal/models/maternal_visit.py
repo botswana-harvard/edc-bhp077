ad85612788def84a32bb4207f24a2c94833b5a9f
melissa kewagamang <mrbkew@gmail.com>
2015-10-21 09:14:32 +0200

    fix maternal visit model

--- microbiome/maternal/models/maternal_visit.py
+++ microbiome/maternal/models/maternal_visit.py
@@ -1,7 +1,26 @@
 from django.db import models
 
+from edc.subject.visit_tracking.models import BaseVisitTracking
+from edc_base.audit_trail import AuditTrail
+from edc_base.model.models import BaseUuidModel
+from edc_consent.models import RequiresConsentMixin
 
-class MaternalVisit(models.Model):
+from maternal_off_study_mixin import MaternalOffStudyMixin
+
+
+class MaternalVisit(MaternalOffStudyMixin, RequiresConsentMixin, BaseVisitTracking, BaseUuidModel):
+
+    """ Maternal visit form that links all antenatal/ postnatal follow-up forms """
+
+    history = AuditTrail(True)
+
+    def __unicode__(self):
+        return '{} {} ({}) {}'.format(self.appointment.registered_subject.subject_identifier,
+                                      self.appointment.registered_subject.first_name,
+                                      self.appointment.registered_subject.gender,
+                                      self.appointment.visit_definition.code)
 
     class Meta:
         app_label = 'maternal'
+        verbose_name = 'Maternal Visit'
+        verbose_name_plural = 'Maternal Visit'
