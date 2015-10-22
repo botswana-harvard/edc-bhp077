6182741103718f82b8c16107a40db148f419d868
melissa kewagamang <mrbkew@gmail.com>
2015-10-22 12:48:34 +0200

    update visit_schedules

--- microbiome/maternal/visit_schedule/maternal_delivery_visit.py
+++ microbiome/maternal/visit_schedule/maternal_delivery_visit.py
@@ -11,14 +11,14 @@ from ..models import MaternalVisit, PostnatalEnrollment
 class MaternalDeliveryVisitSchedule(VisitScheduleConfiguration):
 
     name = 'delivery visit schedule'
-    app_label = 'microbiome'
+    app_label = 'maternal'
 
-    membership_forms = OrderedDict({'postnatal': MembershipFormTuple(
-        'postnatal', PostnatalEnrollment, True), })
+    membership_forms = OrderedDict({'childbirth': MembershipFormTuple(
+        'childbirth', PostnatalEnrollment, True), })
 
     schedule_groups = OrderedDict({
-        'delivery': ScheduleGroupTuple(
-            'delivery', 'postnatal', '', None), })
+        'delivery': ScheduleGroupTuple('delivery',
+                                       'childbirth', '', None), })
 
     visit_definitions = OrderedDict(
         {'2000M': {
@@ -35,28 +35,27 @@ class MaternalDeliveryVisitSchedule(VisitScheduleConfiguration):
             'schedule_group': 'delivery',
             'instructions': '',
             'requisitions': (
-#                 RequisitionPanelTuple(100L, u'microbiome', u'maternalrequisition',
-#                                       'Viral Load', 'TEST', 'WB', NOT_REQUIRED, ADDITIONAL),
-#                 RequisitionPanelTuple(200L, u'microbiome', u'maternalrequisition',
-#                                       'Breast Milk (Storage)', 'STORAGE', 'BM', REQUIRED, NOT_ADDITIONAL),
-#                 RequisitionPanelTuple(300L, u'microbiome', u'maternalrequisition',
-#                                       'Vaginal swab (Storage)', 'STORAGE', 'VS', REQUIRED, NOT_ADDITIONAL),
-#                 RequisitionPanelTuple(400L, u'microbiome', u'maternalrequisition',
-#                                       'Rectal swab (Storage)', 'STORAGE', 'RS', REQUIRED, NOT_ADDITIONAL),
-#                 RequisitionPanelTuple(500L, u'microbiome', u'maternalrequisition',
-#                                       'Skin Flora (Storage)', 'STORAGE', 'SF', REQUIRED, NOT_ADDITIONAL),
-#                 RequisitionPanelTuple(600L, u'microbiome', u'maternalrequisition',
-#                                       'Vaginal Swab (multiplex PCR)', 'TEST', 'VSM', REQUIRED, NOT_ADDITIONAL)
-                ),
+                RequisitionPanelTuple(10L, u'microbiome_lab', u'maternalrequisition',
+                                      'Viral Load', 'TEST', 'WB', NOT_REQUIRED, ADDITIONAL),
+                RequisitionPanelTuple(20L, u'microbiome_lab', u'maternalrequisition',
+                                      'Breast Milk (Storage)', 'STORAGE', 'BM', REQUIRED, NOT_ADDITIONAL),
+                RequisitionPanelTuple(30L, u'microbiome_lab', u'maternalrequisition',
+                                      'Vaginal swab (Storage)', 'STORAGE', 'VS', REQUIRED, NOT_ADDITIONAL),
+                RequisitionPanelTuple(40L, u'microbiome_lab', u'maternalrequisition',
+                                      'Rectal swab (Storage)', 'STORAGE', 'RS', REQUIRED, NOT_ADDITIONAL),
+                RequisitionPanelTuple(50L, u'microbiome_lab', u'maternalrequisition',
+                                      'Skin Flora (Storage)', 'STORAGE', 'SF', REQUIRED, NOT_ADDITIONAL),
+                RequisitionPanelTuple(60L, u'microbiome_lab', u'maternalrequisition',
+                                      'Vaginal Swab (multiplex PCR)', 'TEST', 'VSM', REQUIRED, NOT_ADDITIONAL)),
             'entries': (
                 EntryTuple(10L, u'maternal', u'maternallabourdel', REQUIRED, NOT_ADDITIONAL),
                 EntryTuple(20L, u'maternal', u'maternallabdelmed', REQUIRED, NOT_ADDITIONAL),
                 EntryTuple(30L, u'maternal', u'maternallabdelclinic', REQUIRED, NOT_ADDITIONAL),
                 EntryTuple(40L, u'maternal', u'maternallabdeldx', REQUIRED, NOT_ADDITIONAL),
                 EntryTuple(50L, u'maternal', u'maternallabdeldxt', REQUIRED, NOT_ADDITIONAL),
-                EntryTuple(60L, u'maternal', u'maternalarvpreg', REQUIRED, NOT_ADDITIONAL),
-                EntryTuple(70L, u'maternal', u'maternalarv', REQUIRED, NOT_ADDITIONAL),
-                # EntryTuple(200L, u'microbiome', u'maternaldeath', NOT_REQUIRED, ADDITIONAL),
+                EntryTuple(60L, u'maternal', u'maternalarvpreg', NOT_REQUIRED, ADDITIONAL),
+                EntryTuple(70L, u'maternal', u'maternalarv', NOT_REQUIRED, ADDITIONAL),
+                EntryTuple(200L, u'maternal', u'maternaldeath', NOT_REQUIRED, ADDITIONAL),
                 EntryTuple(210L, u'maternal', u'maternaloffstudy', NOT_REQUIRED, ADDITIONAL), )}}
     )
 site_visit_schedules.register(MaternalDeliveryVisitSchedule)
