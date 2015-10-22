7a22230fe66f9d093e368f924ec7f62a0400a49b
melissa kewagamang <mrbkew@gmail.com>
2015-10-22 12:46:38 +0200

    add postnatal visits

---
+++ microbiome/maternal/visit_schedule/maternal_postnatal_visit.py
@@ -0,0 +1,155 @@
+from collections import OrderedDict
+
+from edc.subject.visit_schedule.classes import (VisitScheduleConfiguration, site_visit_schedules,
+                                                EntryTuple, MembershipFormTuple,
+                                                ScheduleGroupTuple, RequisitionPanelTuple)
+from edc.constants import REQUIRED, NOT_REQUIRED, ADDITIONAL, NOT_ADDITIONAL
+
+from ..models import MaternalVisit, PostnatalEnrollment
+
+
+class MaternalPostnatalVisitSchedule(VisitScheduleConfiguration):
+
+    name = 'postnatal visit'
+    app_label = 'maternal'
+
+    membership_forms = OrderedDict({
+        'after_birth': MembershipFormTuple('after_birth', PostnatalEnrollment, True), })
+
+    schedule_groups = OrderedDict({
+        'Post Partum Follow-up': ScheduleGroupTuple('Post Partum Follow-up',
+                                                    'after_birth', None, None), })
+
+    visit_definitions = OrderedDict()
+
+    visit_definitions['2010M'] = {
+        'title': '1 Month Visit',
+        'time_point': 10,
+        'base_interval': 1,
+        'base_interval_unit': 'M',
+        'window_lower_bound': 0,
+        'window_lower_bound_unit': 'D',
+        'window_upper_bound': 0,
+        'window_upper_bound_unit': 'D',
+        'grouping': '',
+        'visit_tracking_model': MaternalVisit,
+        'schedule_group': 'Post Partum Follow-up',
+        'instructions': None,
+        'requisitions': (
+            RequisitionPanelTuple(10L, u'microbiome_lab', u'maternalrequisition', 'Viral Load', 'TEST', 'WB', REQUIRED, NOT_ADDITIONAL),
+            RequisitionPanelTuple(20L, u'microbiome_lab', u'maternalrequisition', 'Breast Milk (Storage)', 'STORAGE', 'BM', NOT_REQUIRED, ADDITIONAL)),
+        'entries': (
+            EntryTuple(10L, u'maternal', u'maternalpostfu', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(20L, u'maternal', u'maternalpostfudx', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(30L, u'maternal', u'sexualreproductivehealth', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(40L, u'maternal', u'maternalarvpost', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(50L, u'maternal', u'maternalarvpostadh', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(60L, u'maternal', u'rapidtestresult', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(200L, u'maternal', u'maternaldeath', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(210L, u'maternal', u'maternaloffstudy', NOT_REQUIRED, ADDITIONAL), )}
+
+    visit_definitions['2030M'] = {
+        'title': '3 Months Visit',
+        'time_point': 30,
+        'base_interval': 3,
+        'base_interval_unit': 'M',
+        'window_lower_bound': 0,
+        'window_lower_bound_unit': 'D',
+        'window_upper_bound': 0,
+        'window_upper_bound_unit': 'D',
+        'grouping': '',
+        'visit_tracking_model': MaternalVisit,
+        'schedule_group': 'Post Partum Follow-up',
+        'instructions': None,
+        'requisitions': (
+            RequisitionPanelTuple(10L, u'microbiome_lab', u'maternalrequisition', 'Viral Load', 'TEST', 'WB', REQUIRED, NOT_ADDITIONAL),
+            RequisitionPanelTuple(20L, u'microbiome_lab', u'maternalrequisition', 'Breast Milk (Storage)', 'STORAGE', 'BM', NOT_REQUIRED, ADDITIONAL)),
+        'entries': (
+            EntryTuple(10L, u'maternal', u'maternalpostfu', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(20L, u'maternal', u'maternalpostfudx', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(30L, u'maternal', u'sexualreproductivehealth', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(40L, u'maternal', u'maternalarvpost', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(50L, u'maternal', u'maternalarvpostadh', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(60L, u'maternal', u'rapidtestresult', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(200L, u'maternal', u'maternaldeath', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(210L, u'maternal', u'maternaloffstudy', NOT_REQUIRED, ADDITIONAL), )}
+
+    visit_definitions['2060M'] = {
+        'title': '6 Months Visit',
+        'time_point': 60,
+        'base_interval': 6,
+        'base_interval_unit': 'M',
+        'window_lower_bound': 0,
+        'window_lower_bound_unit': 'D',
+        'window_upper_bound': 0,
+        'window_upper_bound_unit': 'D',
+        'grouping': '',
+        'visit_tracking_model': MaternalVisit,
+        'schedule_group': 'Post Partum Follow-up',
+        'instructions': None,
+        'requisitions': (
+            RequisitionPanelTuple(10L, u'microbiome_lab', u'maternalrequisition', 'Viral Load', 'TEST', 'WB', REQUIRED, NOT_ADDITIONAL),
+            RequisitionPanelTuple(20L, u'microbiome_lab', u'maternalrequisition', 'Breast Milk (Storage)', 'STORAGE', 'BM', NOT_REQUIRED, ADDITIONAL)),
+        'entries': (
+            EntryTuple(10L, u'maternal', u'maternalpostfu', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(20L, u'maternal', u'maternalpostfudx', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(30L, u'maternal', u'sexualreproductivehealth', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(40L, u'maternal', u'maternalarvpost', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(50L, u'maternal', u'maternalarvpostadh', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(60L, u'maternal', u'rapidtestresult', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(200L, u'maternal', u'maternaldeath', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(210L, u'maternal', u'maternaloffstudy', NOT_REQUIRED, ADDITIONAL), )}
+
+    visit_definitions['2090M'] = {
+        'title': '9 Months Visit',
+        'time_point': 90,
+        'base_interval': 9,
+        'base_interval_unit': 'M',
+        'window_lower_bound': 0,
+        'window_lower_bound_unit': 'D',
+        'window_upper_bound': 0,
+        'window_upper_bound_unit': 'D',
+        'grouping': '',
+        'visit_tracking_model': MaternalVisit,
+        'schedule_group': 'Post Partum Follow-up',
+        'instructions': None,
+        'requisitions': (
+            RequisitionPanelTuple(10L, u'microbiome_lab', u'maternalrequisition', 'Viral Load', 'TEST', 'WB', REQUIRED, NOT_ADDITIONAL),
+            RequisitionPanelTuple(20L, u'microbiome_lab', u'maternalrequisition', 'Breast Milk (Storage)', 'STORAGE', 'BM', NOT_REQUIRED, ADDITIONAL)),
+        'entries': (
+            EntryTuple(10L, u'maternal', u'maternalpostfu', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(20L, u'maternal', u'maternalpostfudx', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(30L, u'maternal', u'sexualreproductivehealth', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(40L, u'maternal', u'maternalarvpost', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(50L, u'maternal', u'maternalarvpostadh', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(60L, u'maternal', u'rapidtestresult', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(200L, u'maternal', u'maternaldeath', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(210L, u'maternal', u'maternaloffstudy', NOT_REQUIRED, ADDITIONAL), )}
+
+    visit_definitions['2120M'] = {
+        'title': '12 Months Visit',
+        'time_point': 120,
+        'base_interval': 12,
+        'base_interval_unit': 'M',
+        'window_lower_bound': 0,
+        'window_lower_bound_unit': 'D',
+        'window_upper_bound': 0,
+        'window_upper_bound_unit': 'D',
+        'grouping': '',
+        'visit_tracking_model': MaternalVisit,
+        'schedule_group': 'Post Partum Follow-up',
+        'instructions': None,
+        'requisitions': (
+            RequisitionPanelTuple(10L, u'microbiome_lab', u'maternalrequisition', 'Viral Load', 'TEST', 'WB', REQUIRED, NOT_ADDITIONAL),
+            RequisitionPanelTuple(20L, u'microbiome_lab', u'maternalrequisition', 'Breast Milk (Storage)', 'STORAGE', 'BM', NOT_REQUIRED, ADDITIONAL)),
+        'entries': (
+            EntryTuple(10L, u'maternal', u'maternalpostfu', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(20L, u'maternal', u'maternalpostfudx', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(30L, u'maternal', u'sexualreproductivehealth', REQUIRED, NOT_ADDITIONAL),
+            EntryTuple(40L, u'maternal', u'maternalarvpost', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(50L, u'maternal', u'maternalarvpostadh', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(60L, u'maternal', u'rapidtestresult', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(200L, u'maternal', u'maternaldeath', NOT_REQUIRED, ADDITIONAL),
+            EntryTuple(210L, u'maternal', u'maternaloffstudy', NOT_REQUIRED, ADDITIONAL), )}
+
+site_visit_schedules.register(MaternalPostnatalVisitSchedule)
