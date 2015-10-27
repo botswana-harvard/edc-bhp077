277491504a0cdb781a6eab54c20ea0ff0e3b8b3c
melissa kewagamang <mrbkew@gmail.com>
2015-10-27 10:58:16 +0200

    dashboard urls

--- microbiome/dashboard/urls.py
+++ microbiome/dashboard/urls.py
@@ -2,12 +2,6 @@ from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from .classes import MaternalDashboard
 
-# regex = {}
-# regex['dashboard_type'] = 'maternal'
-# regex['dashboard_model'] = 'maternal_consent'
-# urlpatterns += MaternalDashboard.get_urlpatterns('microbiome.dashboard.views',
-#                                                  regex, visit_field_names=['maternal_visit', ])
-
 urlpatterns = []
 
 for pattern in MaternalDashboard.get_urlpatterns():
