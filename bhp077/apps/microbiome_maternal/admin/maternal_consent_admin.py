f090e5c8dc30748b1984ff3bc1acc9d6d1d3b3ac
melissa kewagamang <mrbkew@gmail.com>
2015-10-28 17:53:41 +0200

    fix consent admin

--- bhp077/apps/microbiome_maternal/admin/maternal_consent_admin.py
+++ bhp077/apps/microbiome_maternal/admin/maternal_consent_admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.core.urlresolvers import reverse
-from django.http.response import HttpResponseRedirect
 
 from edc_base.modeladmin.admin import BaseModelAdmin
 
@@ -34,31 +32,5 @@ class MaternalConsentAdmin(BaseModelAdmin):
                     'gender': admin.VERTICAL,
                     'is_dob_estimated': admin.VERTICAL,
                     'identity_type': admin.VERTICAL}
-
-    def reverse_next_to_dashboard(self, next_url_name, request, obj, **kwargs):
-        pass
-
-    def response_add(self, request, obj, post_url_continue=None):
-        """Redirects as default unless keyword 'next' is in the GET and is a
-        valid url_name (e.g. can be reversed using other GET values)."""
-        http_response_redirect = super(MaternalConsent, self).response_add(request, obj, post_url_continue)
-        custom_http_response_redirect = None
-        if '_addanother' not in request.POST and '_continue' not in request.POST:
-            if request.GET.get('next'):
-                custom_http_response_redirect = self.response_add_redirect_on_next_url(
-                    request.GET.get('next'),
-                    request,
-                    obj,
-                    post_url_continue,
-                    post_save=request.POST.get('_save'),
-                    post_save_next=request.POST.get('_savenext'),
-                    post_cancel=request.POST.get('_cancel'))
-        return custom_http_response_redirect or http_response_redirect
-
-    def response_add_redirect_on_next_url(
-            self, next_url_name, request, obj, post_url_continue, post_save=None,
-            post_save_next=None, post_cancel=None):
-            url = ('{0}?dashboard_id={1}').format(reverse('dashboard_id_url', kwargs={'dashboard_id': obj.id}), obj.id)
-            return HttpResponseRedirect(url)
 
 admin.site.register(MaternalConsent, MaternalConsentAdmin)
