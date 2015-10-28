5363101f8c269fffa935721a4f8e6b5c0cd982f5
melissa kewagamang <mrbkew@gmail.com>
2015-10-28 17:15:37 +0200

    maternal eligibility validation

--- bhp077/apps/microbiome_maternal/forms/maternal_eligibility_form.py
+++ bhp077/apps/microbiome_maternal/forms/maternal_eligibility_form.py
@@ -1,9 +1,20 @@
+from django import forms
 from django.forms import ModelForm
+from edc_constants.constants import YES
 
 from ..models import MaternalEligibility
 
 
 class MaternalEligibilityForm(ModelForm):
+
+    def clean(self):
+
+        cleaned_data = self.cleaned_data
+        if cleaned_data.get('currently_pregnant') == YES and cleaned_data.get('recently_delivered') == YES:
+            raise forms.ValidationError('Participant CANNOT BE BOTH: pregnant & just delivered.'
+                                        'Please Correct.')
+        cleaned_data = super(MaternalEligibilityForm, self).clean()
+        return cleaned_data
 
     class Meta:
         model = MaternalEligibility
