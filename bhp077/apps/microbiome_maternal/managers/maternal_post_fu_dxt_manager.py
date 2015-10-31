0cdc5bebd32a682cbb6a359cc80573a32385579e
melissa kewagamang <mrbkew@gmail.com>
2015-10-31 12:45:29 +0200

    fix get_model on managers

--- bhp077/apps/microbiome_maternal/managers/maternal_post_fu_dxt_manager.py
+++ bhp077/apps/microbiome_maternal/managers/maternal_post_fu_dxt_manager.py
@@ -4,8 +4,8 @@ from django.db import models
 class MaternalPostFuDxTManager(models.Manager):
 
     def get_by_natural_key(self, report_datetime, visit_instance, appt_status, visit_definition_code, subject_identifier_as_pk):
-        MaternalVisit = models.get_model('maternal', 'MaternalVisit')
-        MaternalPostFuDx = models.get_model('maternal', 'MaternalPostFuDx')
+        MaternalVisit = models.get_model('microbiome_maternal', 'MaternalVisit')
+        MaternalPostFuDx = models.get_model('microbiome_maternal', 'MaternalPostFuDx')
         maternal_visit = MaternalVisit.objects.get_by_natural_key(report_datetime,
                                                                   visit_instance,
                                                                   appt_status,
