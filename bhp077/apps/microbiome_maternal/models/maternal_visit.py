276e310a2332e25cf48f22d597e58c4cb4560378
melissa kewagamang <mrbkew@gmail.com>
2015-11-03 15:25:14 +0200

    add create_additional_maternal_forms_meta method

--- bhp077/apps/microbiome_maternal/models/maternal_visit.py
+++ bhp077/apps/microbiome_maternal/models/maternal_visit.py
@@ -1,14 +1,16 @@
 from django.core.urlresolvers import reverse
 from django.db import models
 
+from edc.entry_meta_data.models import ScheduledEntryMetaData
+from edc.subject.entry.models import Entry
 from edc.subject.visit_tracking.models import BaseVisitTracking
-from edc_base.model.models import BaseUuidModel
 from edc_base.audit_trail import AuditTrail
+from edc_base.model.models import BaseUuidModel
 from edc_consent.models import RequiresConsentMixin
-from bhp077.apps.microbiome_maternal.models import MaternalConsent
 
-from bhp077.apps.microbiome.choices import VISIT_UNSCHEDULED_REASON, VISIT_REASON
 from .maternal_off_study_mixin import MaternalOffStudyMixin
+from bhp077.apps.microbiome.choices import VISIT_UNSCHEDULED_REASON, VISIT_REASON
+from bhp077.apps.microbiome_maternal.models import MaternalConsent
 
 
 class MaternalVisit(MaternalOffStudyMixin, RequiresConsentMixin, BaseVisitTracking, BaseUuidModel):
@@ -30,17 +32,55 @@ class MaternalVisit(MaternalOffStudyMixin, RequiresConsentMixin, BaseVisitTracki
         return reverse('admin:microbiome_maternal_maternalvisit_add', args=(self.id,))
 
     def __unicode__(self):
-        return '{} {} ({}) {}'.format(self.appointment.registered_subject.subject_identifier,
-                                      self.appointment.registered_subject.first_name,
-                                      self.appointment.registered_subject.gender,
-                                      self.appointment.visit_definition.code)
+        return '{} {} {}'.format(self.appointment.registered_subject.subject_identifier,
+                                 self.appointment.registered_subject.first_name,
+                                 self.appointment.visit_definition.code)
 
     def get_visit_reason_choices(self):
         return VISIT_REASON
 
     def save(self, *args, **kwargs):
         self.subject_identifier = self.appointment.registered_subject.subject_identifier
+        self.create_additional_maternal_forms_meta()
         super(MaternalVisit, self).save(*args, **kwargs)
+
+    def create_additional_maternal_forms_meta(self):
+        if self.reason == 'off study':
+            entry = Entry.objects.filter(
+                model_name='maternaloffstudy',
+                visit_definition_id=self.appointment.visit_definition_id)
+            if entry:
+                scheduled_meta_data = ScheduledEntryMetaData.objects.filter(
+                    appointment=self.appointment,
+                    entry=entry[0],
+                    registered_subject=self.registered_subject)
+                if not scheduled_meta_data:
+                    scheduled_meta_data = ScheduledEntryMetaData.objects.create(
+                        appointment=self.appointment,
+                        entry=entry[0],
+                        registered_subject=self.registered_subject)
+                else:
+                    scheduled_meta_data = scheduled_meta_data[0]
+                scheduled_meta_data.entry_status = 'NEW'
+                scheduled_meta_data.save()
+        if self.reason == 'death':
+            entries = Entry.objects.filter(
+                model_name__in=['maternaldeath', 'maternaloffstudy'],
+                visit_definition_id=self.appointment.visit_definition_id)
+            for entry in entries:
+                scheduled_meta_data = ScheduledEntryMetaData.objects.filter(
+                    appointment=self.appointment,
+                    entry=entry[0],
+                    registered_subject=self.registered_subject)
+                if not scheduled_meta_data:
+                    scheduled_meta_data = ScheduledEntryMetaData.objects.create(
+                        appointment=self.appointment,
+                        entry=entry[0],
+                        registered_subject=self.registered_subject)
+                else:
+                    scheduled_meta_data = scheduled_meta_data[0]
+                scheduled_meta_data.entry_status = 'NEW'
+                scheduled_meta_data.save()
 
     class Meta:
         app_label = 'microbiome_maternal'
