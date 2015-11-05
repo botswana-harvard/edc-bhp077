d05f021f95dd9a35c613a7190752248b36998d76
melissa kewagamang <mrbkew@gmail.com>
2015-11-05 12:02:19 +0200

    infant off study admin

--- bhp077/apps/microbiome_infant/admin/infant_off_study_admin.py
+++ bhp077/apps/microbiome_infant/admin/infant_off_study_admin.py
@@ -7,13 +7,10 @@ class InfantOffStudyAdmin(admin.ModelAdmin):
 
     list_display = (
         'infant_visit',
-        'report_datetime',
         'offstudy_date',
         'reason',
         'has_scheduled_data'
     )
-
-    list_filter = ('report_datetime', 'offstudy_date')
 
     radio_fields = {'has_scheduled_data': admin.VERTICAL}
 
