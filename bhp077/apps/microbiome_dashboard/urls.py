4e43a7d6741ded6179a9c160f9c1ab09a2060c62
melissa kewagamang <mrbkew@gmail.com>
2015-11-06 16:14:01 +0200

    add infant url

--- bhp077/apps/microbiome_dashboard/urls.py
+++ bhp077/apps/microbiome_dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from .classes import MaternalDashboard
+from .classes import MaternalDashboard, InfantDashboard
 
 urlpatterns = []
 
@@ -9,3 +9,9 @@ for pattern in MaternalDashboard.get_urlpatterns():
         url(pattern,
             login_required(MaternalDashboard.as_view()),
             name=MaternalDashboard.dashboard_url_name))
+
+for pattern in InfantDashboard.get_urlpatterns():
+    urlpatterns.append(
+        url(pattern,
+            login_required(InfantDashboard.as_view()),
+            name=InfantDashboard.dashboard_url_name))
