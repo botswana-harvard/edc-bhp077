351622e044ada32a83025621dae05f85dd44d1f6
Florence Chilisa <fchilisa@gmail.com>
2015-11-12 10:53:02 +0200

    maternal validations

--- bhp077/apps/microbiome_maternal/forms/antenatal_enrollment_form.py
+++ bhp077/apps/microbiome_maternal/forms/antenatal_enrollment_form.py
@@ -1,9 +1,22 @@
-from edc.base.form.forms import BaseModelForm
+from django import forms
+
+from edc_constants.constants import NOT_APPLICABLE
 
 from ..models import AntenatalEnrollment
 
+from .base_enrollment_form import BaseEnrollmentForm
 
-class AntenatalEnrollmentForm(BaseModelForm):
+
+class AntenatalEnrollmentForm(BaseEnrollmentForm):
+
+    def clean(self):
+        cleaned_data = super(AntenatalEnrollmentForm, self).clean()
+        # If weeks of gestation are greater than 32 weeks, do a rapid test
+        if cleaned_data.get('weeks_of_gestation') >= 32:
+            if cleaned_data.get('process_rapid_test') == NOT_APPLICABLE:
+                raise forms.ValidationError('Weeks of gestation are greater or equal to 32. Rapid test processing '
+                                            'CANNOT be Not Applicable (Rapid test is expected). Please correct.')
+        return cleaned_data
 
     class Meta:
         model = AntenatalEnrollment
