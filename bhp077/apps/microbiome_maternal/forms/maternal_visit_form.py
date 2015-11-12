d011545e331782d64382bc27240c8ff7f311c6ba
melissa kewagamang <mrbkew@gmail.com>
2015-11-12 11:26:15 +0200

    visit form

--- bhp077/apps/microbiome_maternal/forms/maternal_visit_form.py
+++ bhp077/apps/microbiome_maternal/forms/maternal_visit_form.py
@@ -3,7 +3,7 @@ from django.contrib.admin.widgets import AdminRadioSelect, AdminRadioFieldRender
 
 from edc.base.form.forms import BaseModelForm
 
-from ..models import MaternalVisit
+from ..models import MaternalVisit, MaternalConsent
 from bhp077.apps.microbiome.choices import VISIT_REASON, VISIT_INFO_SOURCE
 
 
@@ -19,6 +19,20 @@ class MaternalVisitForm (BaseModelForm):
         label='Source of information',
         choices=[choice for choice in VISIT_INFO_SOURCE],
         widget=AdminRadioSelect(renderer=AdminRadioFieldRenderer))
+
+    def clean(self):
+
+        cleaned_data = self.cleaned_data
+        maternal_consent = MaternalConsent.objects.filter(
+            registered_subject__subject_identifier=cleaned_data.get('maternal_visit').appointment.registered_subject.subject_identifier)
+        if not cleaned_data.get("report_datetime"):
+            raise forms.ValidationError("Please fill in the date and time")
+        if cleaned_data.get("report_datetime") < maternal_consent.consent_datetime:
+            raise forms.ValidationError("Report datetime CANNOT be before consent_datetime")
+        if cleaned_data.get("report_datetime") < maternal_consent.dob:
+            raise forms.ValidationError("Report datetime CANNOT be before DOB")
+
+        super(MaternalVisitForm, self).clean()
 
     class Meta:
         model = MaternalVisit
