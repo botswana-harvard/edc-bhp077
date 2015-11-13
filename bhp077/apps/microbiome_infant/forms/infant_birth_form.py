6967e16a1a7fcebf7b78ee70c8eb5fe86eced6fa
Florence Chilisa <fchilisa@gmail.com>
2015-11-13 11:38:55 +0200

    Infant birth validations

--- bhp077/apps/microbiome_infant/forms/infant_birth_form.py
+++ bhp077/apps/microbiome_infant/forms/infant_birth_form.py
@@ -1,9 +1,29 @@
-from ..models import InfantBirth
+from django import forms
 
 from edc.base.form.forms import BaseModelForm
 
+from bhp077.apps.microbiome_maternal.models import MaternalLabourDel
+
+from ..models import InfantBirth
+
 
 class InfantBirthForm(BaseModelForm):
+
+    def clean(self):
+        cleaned_data = super(InfantBirthForm, self).clean()
+        # does dob match maternal lab del?
+        maternal_identifier = cleaned_data.get('registered_subject', None).relative_identifier
+        try:
+            maternal_lab_del = MaternalLabourDel.objects.get(
+                maternal_visit__appointment__registered_subject__subject_identifier=maternal_identifier)
+            if not cleaned_data.get('dob', None) == maternal_lab_del.delivery_datetime.date():
+                raise forms.ValidationError('Infant dob must match maternal delivery date of {}. You wrote {}'
+                                            .format(maternal_lab_del.delivery_datetime.date(),
+                                                    cleaned_data.get('dob', None)))
+        except MaternalLabourDel.DoesNotExist:
+            raise forms.ValidationError('Cannot find maternal labour and delivery form for this infant!'
+                                        ' This is not expected.')
+        return cleaned_data
 
     class Meta:
         model = InfantBirth
