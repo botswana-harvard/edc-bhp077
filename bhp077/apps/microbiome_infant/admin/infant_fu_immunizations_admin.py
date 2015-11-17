aa47e034970c7554ed310bfe4ac0df02d6703cb8
melissa kewagamang <mrbkew@gmail.com>
2015-11-17 17:17:19 +0200

    fix infant immunization form fixes #1507

--- bhp077/apps/microbiome_infant/admin/infant_fu_immunizations_admin.py
+++ bhp077/apps/microbiome_infant/admin/infant_fu_immunizations_admin.py
@@ -1,34 +1,24 @@
 from django.contrib import admin
 
-from ..models import InfantFuImmunizations
-from edc.base.modeladmin.admin import BaseModelAdmin
+from edc_base.modeladmin.admin import BaseModelAdmin, BaseTabularInline
 
-class InfantFuImmunizationsAdmin(admin.ModelAdmin):
+from ..forms import InfantFuImmunizationsForm
+from ..models import InfantFuImmunizations, VaccinesReceived, VaccinesMissed
 
-    list_display = ('vaccines_received', 'vitamin_a_vaccine', 'pneumonia_conjugated_vaccine')
 
-    radio_fields = {
-        'vaccines_received': admin.VERTICAL,
-        'vitamin_a_vaccine': admin.VERTICAL,
-        'reason_not_received_vita_a': admin.VERTICAL,
-        'bcg_vaccine': admin.VERTICAL,
-        'reason_not_received_bcg': admin.VERTICAL,
-        'hepatitis_b_vaccine': admin.VERTICAL,
-        'reason_not_received_hepatitis_b': admin.VERTICAL,
-        'dpt_vaccine': admin.VERTICAL,
-        'reason_not_received_dpt': admin.VERTICAL,
-        'haemophilus_influenza_b_vaccine': admin.VERTICAL,
-        'reason_not_received_haemophilus': admin.VERTICAL,
-        'pneumonia_conjugated_vaccine': admin.VERTICAL,
-        'reason_not_received_pcv': admin.VERTICAL,
-        'polio_vaccine': admin.VERTICAL,
-        'reason_not_received_polio': admin.VERTICAL,
-        'rotavirus_vaccine': admin.VERTICAL,
-        'reason_not_received_rotavirus': admin.VERTICAL,
-        'measles_vaccine': admin.VERTICAL,
-        'reason_not_received_measles': admin.VERTICAL,
-        'pentavalent_vaccine': admin.VERTICAL,
-        'reason_not_received_pentavalent': admin.VERTICAL,
-    }
+class VaccinesReceivedInlineAdmin(BaseTabularInline):
+    model = VaccinesReceived
+    extra = 1
 
+
+class VaccinesMissedInlineAdmin(BaseTabularInline):
+    model = VaccinesMissed
+    extra = 1
+
+
+class InfantFuImmunizationsAdmin(BaseModelAdmin):
+    form = InfantFuImmunizationsForm
+    inlines = [VaccinesReceivedInlineAdmin, VaccinesMissedInlineAdmin, ]
+    radio_fields = {'vaccines_received': admin.VERTICAL,
+                    'vaccines_missed': admin.VERTICAL}
 admin.site.register(InfantFuImmunizations, InfantFuImmunizationsAdmin)
