1b719b768336526d1e5d241e9b2de59336764cf1
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-11-17 17:34:27 +0200

    infant flow up medication inline

--- bhp077/apps/microbiome_infant/admin/infant_fu_new_med_admin.py
+++ bhp077/apps/microbiome_infant/admin/infant_fu_new_med_admin.py
@@ -4,9 +4,13 @@ from ..models import InfantFuNewMed
 
 from edc.base.modeladmin.admin import BaseModelAdmin
 
+from .infant_fu_new_med_items_admin import InfantFuNewMedItemsAdmin
+
 
 class InfantFuNewMedAdmin(BaseModelAdmin):
 
     radio_fields = {'new_medications': admin.VERTICAL, }
+
+    inlines = [InfantFuNewMedItemsAdmin, ]
 
 admin.site.register(InfantFuNewMed, InfantFuNewMedAdmin)
