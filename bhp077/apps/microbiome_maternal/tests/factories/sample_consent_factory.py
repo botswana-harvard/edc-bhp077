5f47bfc8e255947b966a2de9190140cdb2d78de1
melissa kewagamang <mrbkew@gmail.com>
2015-11-17 14:12:23 +0200

    sample consent factory + test

---
+++ bhp077/apps/microbiome_maternal/tests/factories/sample_consent_factory.py
@@ -0,0 +1,21 @@
+import factory
+
+from django.conf import settings
+from django.utils import timezone
+from edc.subject.registration.tests.factories import RegisteredSubjectFactory
+from edc_constants.constants import YES
+
+from bhp077.apps.microbiome_maternal.models import SampleConsent
+
+
+class SampleConsentFactory(factory.DjangoModelFactory):
+
+    class Meta:
+        model = SampleConsent
+
+    registered_subject = factory.SubFactory(RegisteredSubjectFactory)
+    report_datetime = timezone.now()
+    language = settings.LANGUAGES,
+    may_store_samples = YES,
+    is_literate = YES,
+    consent_benefits = YES
