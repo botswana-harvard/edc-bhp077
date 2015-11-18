84838f80b4093f64b23ebef8d4e90c2834b4eb84
melissa kewagamang <mrbkew@gmail.com>
2015-11-18 21:57:51 +0200

    labeling fixes #1512

---
+++ bhp077/config/labels/aliquot_label.py
@@ -0,0 +1,17 @@
+from lis.labeling.classes import ZplTemplateTuple
+
+"""Added here to override the default in app_configuration."""
+
+aliquot_label = ZplTemplateTuple(
+    'aliquot_label', (
+        ('^XA\n'
+         '~SD22'
+         '^FO310,15^A0N,20,20^FD${protocol} Site ${site} ${clinician_initials}   ${aliquot_type} ${aliquot_count}${primary}^FS\n'
+         '^FO310,34^BY1,3.0^BCN,50,N,N,N\n'
+         '^BY^FD${aliquot_identifier}^FS\n'
+         '^FO310,92^A0N,20,20^FD${aliquot_identifier}^FS\n'
+         '^FO310,112^A0N,20,20^FD${subject_identifier} (${initials})^FS\n'
+         '^FO310,132^A0N,20,20^FDDOB: ${dob} ${gender}^FS\n'
+         '^FO310,152^A0N,25,20^FD${drawn_datetime}^FS\n'
+         '^XZ')
+    ), True)
