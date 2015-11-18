60baa855dc1d4359c7fcada408522a3d27504e39
Florence Chilisa <fchilisa@gmail.com>
2015-11-18 15:28:49 +0200

    fix post fu form

--- bhp077/apps/microbiome_maternal/forms/maternal_post_fu_form.py
+++ bhp077/apps/microbiome_maternal/forms/maternal_post_fu_form.py
@@ -22,18 +22,15 @@ class MaternalPostFuForm(BaseMaternalModelForm):
                 leading=cleaned_data.get('has_chronic_cond'),
                 m2m=cleaned_data.get('chronic_cond'),
                 other=cleaned_data.get('chronic_cond_other'))
-        return cleaned_data
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
 
         if cleaned_data.get('systolic_bp') < cleaned_data.get('diastolic_bp'):
-            forms.ValidationError('Systolic blood pressure can never be less than diastolic blood pressure, please check and correct.')
+            forms.ValidationError('Systolic blood pressure can never be less than diastolic blood pressure, '
+                                  'please check and correct.')
         if cleaned_data.get('systolic_bp') < 75 or cleaned_data.get('systolic_bp') > 175:
             forms.ValidationError('Systolic blood pressure should be between 75 and 175, please check and correct.')
         if cleaned_data.get('diastolic_bp') < 35 or cleaned_data.get('diastolic_bp') > 130:
             forms.ValidationError('Diastolic blood pressure should be between 35 and 130, please check and correct.')
-        return super(MaternalPostFuForm, self).clean()
+        return cleaned_data
 
     class Meta:
         model = MaternalPostFu
