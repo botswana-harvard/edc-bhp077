69be937e4704f149eef43eaa5bfe464488274974
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-11-18 10:34:07 +0200

    infant_fu_new med inline

--- bhp077/apps/microbiome_infant/admin/infant_fu_new_med_admin.py
+++ bhp077/apps/microbiome_infant/admin/infant_fu_new_med_admin.py
@@ -3,14 +3,22 @@ from django.contrib import admin
 from ..models import InfantFuNewMed
 
 from edc.base.modeladmin.admin import BaseModelAdmin
+from edc.base.modeladmin.admin import BaseTabularInline
 
 from .infant_fu_new_med_items_admin import InfantFuNewMedItemsAdmin
+from bhp077.apps.microbiome_infant.forms import InfantFuNewMedItemsForm
+from bhp077.apps.microbiome_infant.models import InfantFuNewMedItems
 
+class InfantFuNewMedItemsInline(BaseTabularInline):
+
+    model = InfantFuNewMedItems
+    form = InfantFuNewMedItemsForm
+    extra = 0
 
 class InfantFuNewMedAdmin(BaseModelAdmin):
 
     radio_fields = {'new_medications': admin.VERTICAL, }
 
-    inlines = [InfantFuNewMedItemsAdmin, ]
+    inlines = [InfantFuNewMedItemsInline, ]
 
 admin.site.register(InfantFuNewMed, InfantFuNewMedAdmin)
