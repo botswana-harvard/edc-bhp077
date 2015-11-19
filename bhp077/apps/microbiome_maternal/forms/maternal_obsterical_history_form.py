dd0aa75974b718ce5acac5a048b5465b9f177ad7
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-11-19 10:26:51 +0200

    maternal obsterical history validations & tests

--- bhp077/apps/microbiome_maternal/forms/maternal_obsterical_history_form.py
+++ bhp077/apps/microbiome_maternal/forms/maternal_obsterical_history_form.py
@@ -8,11 +8,25 @@ class MaternalObstericalHistoryForm(BaseMaternalModelForm):
 
     def clean(self):
         cleaned_data = self.cleaned_data
-        if cleaned_data.get('lost_after_24wks') != cleaned_data.get('children_died_b4_5yrs') and cleaned_data.get('children_died_b4_5yrs') != cleaned_data.get('pregs_24wks_or_more'):
+
+        if cleaned_data.get('pregs_24wks_or_more') < 0:
+            raise forms.ValidationError('Number of pregnancies at least 24 weeks, should be greater than zero.')
+        else:
+            if cleaned_data.get('live_children')  < 0:
+                raise forms.ValidationError('How many other living children does the participant currently'
+                ' have (excluding baby to be enrolled in the study.)'
+                ' Provide value greater than zero.')
+            if cleaned_data.get('children_died_b4_5yrs') < 0:
+                raise forms.ValidationError("How many of the participant's children died after birth before 5 years of age?."
+                " Provide value greater than zero.")
+
+        if cleaned_data.get('lost_after_24wks') != cleaned_data.get('children_died_b4_5yrs') and \
+                        cleaned_data.get('children_died_b4_5yrs') != cleaned_data.get('pregs_24wks_or_more'):
             raise forms.ValidationError(
                 'Number of pregnancies at least 24 weeks and '
                 'Number of pregnancies lost at or after 24 weeks gestation and '
                 'children died after birth before 5 years of age should be the same.')
+        return cleaned_data
 
     class Meta:
         model = MaternalObstericalHistory
