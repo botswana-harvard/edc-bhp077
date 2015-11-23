b4baf3a3e76af66dcdf0e243fdea341d8e8d3f9d
melissa kewagamang <mrbkew@gmail.com>
2015-11-23 09:21:03 +0200

    move recruitment info to consent fixes #1537

--- bhp077/apps/microbiome_maternal/admin/maternal_uninfected_admin.py
+++ bhp077/apps/microbiome_maternal/admin/maternal_uninfected_admin.py
@@ -8,21 +8,9 @@ from ..models import MaternalUninfected
 class MaternalUninfectedAdmin(BaseModelAdmin):
 
     form = MaternalUninfectedForm
-    fields = ('recruit_source',
-              'recruit_source_other',
-              'recruitment_clinic',
-              'recruitment_clinic_other',
-              'prev_pregnancies',
+    fields = ('prev_pregnancies',
               'weight',
               'height',
               'systolic_bp',
               'diastolic_bp')
-    list_display = ('recruit_source',
-                    'recruitment_clinic',
-                    'prev_pregnancies', )
-    list_filter = ('recruit_source',
-                   'recruitment_clinic',
-                   'prev_pregnancies')
-    radio_fields = {'recruit_source': admin.VERTICAL,
-                    'recruitment_clinic': admin.VERTICAL}
 admin.site.register(MaternalUninfected, MaternalUninfectedAdmin)
