a35190d7cc6a6eb42c9f1656dbb32799f915ba49
melissa kewagamang <mrbkew@gmail.com>
2015-11-24 11:01:28 +0200

    update dashboard template

--- bhp077/apps/microbiome_maternal/tests/functional_tests/microbiome_base_page.py
+++ bhp077/apps/microbiome_maternal/tests/functional_tests/microbiome_base_page.py
@@ -1,38 +1,38 @@
-import time
-
-from selenium.webdriver.common.action_chains import ActionChains
-
-
-class MicrobiomeBasePage(object):
-
-    '''This is the base page that carries attributes for every microbiome page'''
-
-    # setting the main page url
-    def __init__(self, driver, base_url='http://0.0.0.0:8000'):
-        self.base_url = base_url
-        self.driver = driver
-        self.timeout = 30
-        time.sleep(1)
-
-    # find elements based on what is defined
-    def find_element(self, *microbiome_finders):
-        return self.driver.find_element(*microbiome_finders)
-
-    # indicate what url to open
-    def open(self, url):
-        url = self.base_url + '/admin/'
-        self.driver.get(url)
-
-    # confirms title on a page
-    def get_title(self):
-        return self.driver.title
-
-    # get url of the current loaded page
-    def get_url(self):
-        return self.driver.current_url
-
-    # getting ready to click on something
-    def mouse_over(self, *microbiome_finders):
-        my_element = self.find_element(*microbiome_finders)
-        mouse_over = ActionChains(self.driver).move_to_element(my_element)
-        mouse_over.perform()
+# import time
+# 
+# from selenium.webdriver.common.action_chains import ActionChains
+# 
+# 
+# class MicrobiomeBasePage(object):
+# 
+#     '''This is the base page that carries attributes for every microbiome page'''
+# 
+#     # setting the main page url
+#     def __init__(self, driver, base_url='http://0.0.0.0:8000'):
+#         self.base_url = base_url
+#         self.driver = driver
+#         self.timeout = 30
+#         time.sleep(1)
+# 
+#     # find elements based on what is defined
+#     def find_element(self, *microbiome_finders):
+#         return self.driver.find_element(*microbiome_finders)
+# 
+#     # indicate what url to open
+#     def open(self, url):
+#         url = self.base_url + '/admin/'
+#         self.driver.get(url)
+# 
+#     # confirms title on a page
+#     def get_title(self):
+#         return self.driver.title
+# 
+#     # get url of the current loaded page
+#     def get_url(self):
+#         return self.driver.current_url
+# 
+#     # getting ready to click on something
+#     def mouse_over(self, *microbiome_finders):
+#         my_element = self.find_element(*microbiome_finders)
+#         mouse_over = ActionChains(self.driver).move_to_element(my_element)
+#         mouse_over.perform()
--- bhp077/apps/microbiome_maternal/tests/functional_tests/microbiome_finders.py
+++ bhp077/apps/microbiome_maternal/tests/functional_tests/microbiome_finders.py
@@ -1,20 +1,20 @@
-from selenium.webdriver.common.by import By
-
-'''Define web objects for Microbiome main pages'''
-
-
-class LoginFinder(object):
-    TITLE = (By.CLASS_NAME, "login")
-    USERNAME = (By.NAME, 'username')
-    PASSWORD = (By.NAME, 'password')
-    SUBMIT = (By.CSS_SELECTOR, "input[type='submit']")
-
-
-class AdminFinder(object):
-    TITLE = (By.CSS_SELECTOR, 'h1')
-    ELIGIBILITY_LINK = (By.LINK_TEXT, 'Maternal Eligibility')
-    ADD_ELIGIBILITY_LINK = (By.LINK_TEXT, 'Add Maternal Eligibility')
-    CONSENT_LINK = (By.LINK_TEXT, 'Maternal Consent')
-    ADD_CONSENT_LINK = (By.LINK_TEXT, 'Add Maternal Consent')
-    SAMPLE_CONSENT_LINK = (By.LINK_TEXT, 'Sample Consent')
-    ADD_SAMPLECONSENT_LINK = (By.CSS_SELECTOR, "a[href*='/admin/microbiome/sampleconsent/add/']")
+# from selenium.webdriver.common.by import By
+# 
+# '''Define web objects for Microbiome main pages'''
+# 
+# 
+# class LoginFinder(object):
+#     TITLE = (By.CLASS_NAME, "login")
+#     USERNAME = (By.NAME, 'username')
+#     PASSWORD = (By.NAME, 'password')
+#     SUBMIT = (By.CSS_SELECTOR, "input[type='submit']")
+# 
+# 
+# class AdminFinder(object):
+#     TITLE = (By.CSS_SELECTOR, 'h1')
+#     ELIGIBILITY_LINK = (By.LINK_TEXT, 'Maternal Eligibility')
+#     ADD_ELIGIBILITY_LINK = (By.LINK_TEXT, 'Add Maternal Eligibility')
+#     CONSENT_LINK = (By.LINK_TEXT, 'Maternal Consent')
+#     ADD_CONSENT_LINK = (By.LINK_TEXT, 'Add Maternal Consent')
+#     SAMPLE_CONSENT_LINK = (By.LINK_TEXT, 'Sample Consent')
+#     ADD_SAMPLECONSENT_LINK = (By.CSS_SELECTOR, "a[href*='/admin/microbiome/sampleconsent/add/']")
