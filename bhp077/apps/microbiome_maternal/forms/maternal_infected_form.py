0e8fbaa044801470a4acb160c697a096fbb53c35
Florence Chilisa <fchilisa@gmail.com>
2015-11-27 12:22:37 +0200

    addional infected validations

--- bhp077/apps/microbiome_maternal/forms/maternal_infected_form.py
+++ bhp077/apps/microbiome_maternal/forms/maternal_infected_form.py
@@ -1,4 +1,8 @@
-from ..models import MaternalInfected
+from django import forms
+
+from edc_constants.constants import NOT_APPLICABLE
+
+from ..models import MaternalInfected, MaternalObstericalHistory
 
 from base_maternal_model_form import BaseMaternalModelForm
 
@@ -7,7 +11,20 @@ class MaternalInfectedForm(BaseMaternalModelForm):
 
     def clean(self):
         cleaned_data = super(MaternalInfectedForm, self).clean()
+        self.validate_prev_preg(cleaned_data)
         return cleaned_data
+
+    def validate_prev_preg(self, cleaned_data):
+        ob_history = MaternalObstericalHistory.objects.filter(
+            maternal_visit__appointment__registered_subject=cleaned_data.get('maternal_visit').appointment.registered_subject)
+        if not ob_history:
+            raise forms.ValidationError('Please fill in the Maternal Obsterical History form first.')
+        else:
+            if ob_history[0].prev_pregnancies == 0:
+                if cleaned_data.get('prev_pregnancy_arv') != NOT_APPLICABLE:
+                    raise forms.ValidationError('In Maternal Obsterical History form you indicated there were no previous '
+                                                'pregnancies. ARVs for PMTCT purposes during previous pregnancy should be '
+                                                'NOT APPLICABLE')
 
     class Meta:
         model = MaternalInfected
