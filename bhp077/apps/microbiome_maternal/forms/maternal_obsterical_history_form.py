e1d6f5b90055341021f4ea8f3b2ee37f78c18d31
Florence Chilisa <fchilisa@gmail.com>
2015-11-27 10:56:55 +0200

    maternal ob history

--- bhp077/apps/microbiome_maternal/forms/maternal_obsterical_history_form.py
+++ bhp077/apps/microbiome_maternal/forms/maternal_obsterical_history_form.py
@@ -7,27 +7,30 @@ from ..models import MaternalObstericalHistory
 class MaternalObstericalHistoryForm(BaseMaternalModelForm):
 
     def clean(self):
-        cleaned_data = self.cleaned_data
+        cleaned_data = super(MaternalObstericalHistoryForm, self).clean()
 
-        if cleaned_data.get('prev_pregnancies') < 0:
-            raise forms.ValidationError('Number of previous pregnancies, should be greater than zero.')
-        if cleaned_data.get('pregs_24wks_or_more') < 0:
-            raise forms.ValidationError('Number of pregnancies at least 24 weeks, should be greater than zero.')
-        else:
-            if cleaned_data.get('live_children')  < 0:
-                raise forms.ValidationError('How many other living children does the participant currently'
-                ' have (excluding baby to be enrolled in the study.)'
-                ' Provide value greater than zero.')
-            if cleaned_data.get('children_died_b4_5yrs') < 0:
-                raise forms.ValidationError("How many of the participant's children died after birth before 5 years of age?."
-                " Provide value greater than zero.")
-
-        if cleaned_data.get('lost_after_24wks') != cleaned_data.get('children_died_b4_5yrs') and \
-                        cleaned_data.get('children_died_b4_5yrs') != cleaned_data.get('pregs_24wks_or_more'):
-            raise forms.ValidationError(
-                'Number of pregnancies at least 24 weeks and '
-                'Number of pregnancies lost at or after 24 weeks gestation and '
-                'children died after birth before 5 years of age should be the same.')
+        if cleaned_data.get('pregs_24wks_or_more') > 0:
+            if (
+                cleaned_data.get('lost_after_24wks') == 0 and
+                cleaned_data.get('live_children') == 0 and
+                cleaned_data.get('children_died_b4_5yrs') == 0
+            ):
+                raise forms.ValidationError('You indicated previous at least 24 weeks were {}. '
+                                            'Number of pregnancies least 24 weeks,'
+                                            'number of pregnancies lost before 24 weeks,'
+                                            'number of pregnancies lost at or after 24 weeks.'
+                                            .format(cleaned_data.get('pregs_24wks_or_more')))
+        if cleaned_data.get('prev_pregnancies') > 0:
+            if (
+                cleaned_data.get('pregs_24wks_or_more') == 0 and
+                cleaned_data.get('lost_before_24wks') == 0 and
+                cleaned_data.get('lost_after_24wks') == 0
+            ):
+                raise forms.ValidationError('You indicated previous pregancies were {}. '
+                                            'Number of pregnancies at or after 24 weeks,'
+                                            'number of living children,'
+                                            'number of children died after 5 year CANNOT all be zero.'
+                                            .format(cleaned_data.get('prev_pregnancies')))
         return cleaned_data
 
     class Meta:
--- bhp077/apps/microbiome_maternal/models/maternal_obsterical_history.py
+++ bhp077/apps/microbiome_maternal/models/maternal_obsterical_history.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 from .maternal_scheduled_visit_model import MaternalScheduledVisitModel
 from .maternal_consent import MaternalConsent
@@ -13,27 +14,33 @@ class MaternalObstericalHistory(MaternalScheduledVisitModel):
 
     prev_pregnancies = models.IntegerField(
         verbose_name="Not including this pregnancy, how many previous pregnancies for this participant?",
+        validators=[MinValueValidator(0), MaxValueValidator(20), ],
         help_text="")
     pregs_24wks_or_more = models.IntegerField(
         verbose_name="Number of pregnancies at least 24 weeks.?",
+        validators=[MinValueValidator(0), MaxValueValidator(20), ],
         help_text="")
 
     lost_before_24wks = models.IntegerField(
         verbose_name="Number of pregnancies lost before 24 weeks gestation",
+        validators=[MinValueValidator(0), MaxValueValidator(20), ],
         help_text="")
 
     lost_after_24wks = models.IntegerField(
         verbose_name="Number of pregnancies lost at or after 24 weeks gestation ",
+        validators=[MinValueValidator(0), MaxValueValidator(20), ],
         help_text="")
 
     live_children = models.IntegerField(
         verbose_name=("How many other living children does the participant currently have"
                       " (excluding baby to be enrolled in the study)"),
+        validators=[MinValueValidator(0), MaxValueValidator(20), ],
         help_text="")
 
     children_died_b4_5yrs = models.IntegerField(
         verbose_name=("How many of the participant's children died after birth before 5"
                       " years of age? "),
+        validators=[MinValueValidator(0), MaxValueValidator(20), ],
         help_text="")
 
     def get_absolute_url(self):
