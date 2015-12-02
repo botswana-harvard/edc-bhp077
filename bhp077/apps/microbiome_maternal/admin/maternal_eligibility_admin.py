a8ce0e241d6b43d003f6ce658c81c624a666c1ec
ckgathi <ckgathi@bhp.org.bw>
2015-12-02 11:11:00 +0200

    add radio fields

--- bhp077/apps/microbiome_maternal/admin/maternal_eligibility_admin.py
+++ bhp077/apps/microbiome_maternal/admin/maternal_eligibility_admin.py
@@ -17,7 +17,8 @@ class MaternalEligibilityAdmin(BaseModelAdmin):
               'currently_pregnant',
               'recently_delivered')
     radio_fields = {'currently_pregnant': admin.VERTICAL,
-                    'recently_delivered': admin.VERTICAL}
+                    'recently_delivered': admin.VERTICAL,
+                    'has_omang': admin.VERTICAL}
     readonly_fields = ('eligibility_id',)
     list_display = ('report_datetime', 'age_in_years', 'is_eligible',
                     'is_consented', 'currently_pregnant', 'recently_delivered')
