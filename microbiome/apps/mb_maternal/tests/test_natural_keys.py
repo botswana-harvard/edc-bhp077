c07b5dd1f224a5a92345a562e9dfff3e48ba39fe
melissa kewagamang <mrbkew@gmail.com>
2015-12-16 18:40:59 +0200

    fix maternal natural key test

--- microbiome/apps/mb_maternal/tests/test_natural_keys.py
+++ microbiome/apps/mb_maternal/tests/test_natural_keys.py
@@ -38,19 +38,21 @@ class NaturalKeyTests(TestCase):
         """Confirms all models have a natural_key method (except Audit models)"""
         app = get_app('mb_maternal')
         for model in get_models(app):
-            if 'Audit' not in model._meta.object_name:
-                self.assertTrue(
-                    'natural_key' in dir(model),
-                    'natural key not found in {0}'.format(model._meta.object_name))
+            if not model._meta.model_name == 'enrollmenttestmodel':
+                if 'Audit' not in model._meta.object_name:
+                    self.assertTrue(
+                        'natural_key' in dir(model),
+                        'natural key not found in {0}'.format(model._meta.object_name))
 
     def test_p2(self):
         """Confirms all models have a get_by_natural_key manager method."""
         app = get_app('mb_maternal')
         for model in get_models(app):
-            if 'Audit' not in model._meta.object_name:
-                self.assertTrue(
-                    'get_by_natural_key' in dir(model.objects),
-                    'get_by_natural_key key not found in {0}'.format(model._meta.object_name))
+            if not model._meta.model_name == 'enrollmenttestmodel':
+                if 'Audit' not in model._meta.object_name:
+                    self.assertTrue(
+                        'get_by_natural_key' in dir(model.objects),
+                        'get_by_natural_key key not found in {0}'.format(model._meta.object_name))
 
     def test_serialisation_deserialize(self):
         """Confirms all models have a get_by_natural_key manager method."""
