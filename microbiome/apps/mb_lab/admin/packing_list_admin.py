f95f6dfa27cb88046e7ff22004175abd31f233bb
erikvw <ew2789@gmail.com>
2016-01-09 19:02:37 +0200

    no need to overwrite base class

--- microbiome/apps/mb_lab/admin/packing_list_admin.py
+++ microbiome/apps/mb_lab/admin/packing_list_admin.py
@@ -10,13 +10,6 @@ class PackingListAdmin(BasePackingListAdmin):
 
     form = PackingListForm
 
-    fields = ('destination', 'list_items', 'list_comment',)
-    list_display = ('reference', 'view_list_items', 'list_datetime',
-                    'list_comment', 'destination', 'received', )
-    list_filter = ('list_datetime', 'destination', 'received', 'list_comment')
-    search_fields = ('id', 'list_comment')
-
-
 admin.site.register(PackingList, PackingListAdmin)
 
 
