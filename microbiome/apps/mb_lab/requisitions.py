97a1278a3426a9063087dd669eb936f4270a720c
erikvw <ew2789@gmail.com>
2016-01-13 14:58:38 +0200

    remove unused site_requisitions, use lab_requisition instead of edc_lab

--- microbiome/apps/mb_lab/requisitions.py
+++
@@ -1,7 +0,0 @@
-from edc_lab.lab_requisition.classes import site_requisitions
-from .models import MaternalRequisition, InfantRequisition
-
-from microbiome.apps.mb.constants import INFANT, MATERNAL
-
-site_requisitions.register(MATERNAL, MaternalRequisition)
-site_requisitions.register(INFANT, InfantRequisition)
