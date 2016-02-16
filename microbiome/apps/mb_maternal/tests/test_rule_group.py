fe9da4d7b8e6ee6cc4af2a7de11cd0a141a3f6ad
Florence Chilisa <fchilisa@gmail.com>
2016-02-16 09:50:16 +0200

    add maternalarvhistory rulegroup

--- microbiome/apps/mb_maternal/tests/test_rule_group.py
+++ microbiome/apps/mb_maternal/tests/test_rule_group.py
@@ -1,10 +1,12 @@
 from django.utils import timezone
 
 from edc_appointment.models import Appointment
-from edc_constants.constants import NEW, YES, POS, NEG, UNKEYED, KEYED, NOT_REQUIRED, NOT_APPLICABLE
+from edc_constants.constants import (NEW, YES, POS, NEG, UNKEYED, KEYED, REQUIRED, NOT_REQUIRED, NOT_APPLICABLE, NO)
 from edc_meta_data.models import CrfMetaData, RequisitionMetaData
 
 from microbiome.apps.mb_maternal.models import RapidTestResult
+
+from ..models import MaternalClinicalHistory
 
 from .base_test_case import BaseTestCase
 from .factories import (
@@ -260,3 +262,120 @@ class TestRuleGroup(BaseTestCase):
                 crf_entry__app_label='mb_maternal',
                 crf_entry__model_name='maternalsrh',
                 appointment=appointment).count(), 1)
+
+    def test_maternal_arv_history_1(self):
+        """Maternal ARV history should be required if prior_health_haart is answered as YES"""
+        self.postnatal_enrollment = PostnatalEnrollmentFactory(
+            registered_subject=self.registered_subject,
+            current_hiv_status=POS,
+            evidence_hiv_status=YES,
+            rapid_test_done=NOT_APPLICABLE,
+            will_breastfeed=YES
+        )
+        self.appointment = Appointment.objects.get(
+            registered_subject=self.registered_subject,
+            visit_definition__code='1000M')
+        self.maternal_visit = MaternalVisitFactory(appointment=self.appointment)
+        MaternalClinicalHistory.objects.create(
+            maternal_visit=self.maternal_visit,
+            prev_preg_azt=YES,
+            prev_sdnvp_labour=NO,
+            prev_preg_haart=YES,
+            lowest_cd4_known=NO,
+            prior_health_haart=YES,
+            prev_pregnancy_arv=NO,
+            know_hiv_status='Nobody',
+        )
+        meta = CrfMetaData.objects.get(
+            crf_entry__app_label='mb_maternal',
+            crf_entry__model_name='maternalarvhistory',
+            appointment=self.appointment)
+        self.assertEqual(meta.entry_status, REQUIRED)
+
+    def test_maternal_arv_history_2(self):
+        """Maternal ARV history should be required if evidence_hiv_status is answered as YES"""
+        self.postnatal_enrollment = PostnatalEnrollmentFactory(
+            registered_subject=self.registered_subject,
+            current_hiv_status=POS,
+            evidence_hiv_status=YES,
+            rapid_test_done=NOT_APPLICABLE,
+            will_breastfeed=YES
+        )
+        self.appointment = Appointment.objects.get(
+            registered_subject=self.registered_subject,
+            visit_definition__code='1000M')
+        self.maternal_visit = MaternalVisitFactory(appointment=self.appointment)
+        MaternalClinicalHistory.objects.create(
+            maternal_visit=self.maternal_visit,
+            prev_preg_azt=YES,
+            prev_sdnvp_labour=NO,
+            prev_preg_haart=YES,
+            lowest_cd4_known=NO,
+            prior_health_haart=NO,
+            prev_pregnancy_arv=YES,
+            know_hiv_status='Nobody',
+        )
+        meta = CrfMetaData.objects.get(
+            crf_entry__app_label='mb_maternal',
+            crf_entry__model_name='maternalarvhistory',
+            appointment=self.appointment)
+        self.assertEqual(meta.entry_status, REQUIRED)
+
+    def test_maternal_arv_history_3(self):
+        """Maternal ARV history should be required if evidence_hiv_status and prior_health_haart is answered as YES"""
+        self.postnatal_enrollment = PostnatalEnrollmentFactory(
+            registered_subject=self.registered_subject,
+            current_hiv_status=POS,
+            evidence_hiv_status=YES,
+            rapid_test_done=NOT_APPLICABLE,
+            will_breastfeed=YES
+        )
+        self.appointment = Appointment.objects.get(
+            registered_subject=self.registered_subject,
+            visit_definition__code='1000M')
+        self.maternal_visit = MaternalVisitFactory(appointment=self.appointment)
+        MaternalClinicalHistory.objects.create(
+            maternal_visit=self.maternal_visit,
+            prev_preg_azt=YES,
+            prev_sdnvp_labour=NO,
+            prev_preg_haart=YES,
+            lowest_cd4_known=NO,
+            prior_health_haart=YES,
+            prev_pregnancy_arv=YES,
+            know_hiv_status='Nobody',
+        )
+        meta = CrfMetaData.objects.get(
+            crf_entry__app_label='mb_maternal',
+            crf_entry__model_name='maternalarvhistory',
+            appointment=self.appointment)
+        self.assertEqual(meta.entry_status, REQUIRED)
+
+    def test_maternal_arv_history_4(self):
+        """Maternal ARV history should NOT be required if evidence_hiv_status and
+        prior_health_haart is answered as NO"""
+        self.postnatal_enrollment = PostnatalEnrollmentFactory(
+            registered_subject=self.registered_subject,
+            current_hiv_status=POS,
+            evidence_hiv_status=YES,
+            rapid_test_done=NOT_APPLICABLE,
+            will_breastfeed=YES
+        )
+        self.appointment = Appointment.objects.get(
+            registered_subject=self.registered_subject,
+            visit_definition__code='1000M')
+        self.maternal_visit = MaternalVisitFactory(appointment=self.appointment)
+        MaternalClinicalHistory.objects.create(
+            maternal_visit=self.maternal_visit,
+            prev_preg_azt=YES,
+            prev_sdnvp_labour=NO,
+            prev_preg_haart=YES,
+            lowest_cd4_known=NO,
+            prior_health_haart=NO,
+            prev_pregnancy_arv=NO,
+            know_hiv_status='Nobody',
+        )
+        meta = CrfMetaData.objects.get(
+            crf_entry__app_label='mb_maternal',
+            crf_entry__model_name='maternalarvhistory',
+            appointment=self.appointment)
+        self.assertEqual(meta.entry_status, NOT_REQUIRED)
--- microbiome/config/labels/aliquot_label.py
+++ microbiome/config/labels/aliquot_label.py
@@ -9,7 +9,7 @@ aliquot_label = ZplTemplateTuple(
          '^FO310,15^A0N,20,20^FD${protocol} Site ${site} ${clinician_initials}   ${aliquot_type} ${aliquot_count}${primary}^FS\n'
          '^FO310,34^BY1,3.0^BCN,50,N,N,N\n'
          '^BY^FD${aliquot_identifier}^FS\n'
-         '^FO310,92^A0N,20,20^FD${aliquot_identifier}^FS\n'
+         '^FO315,92^A0N,20,20^FD${aliquot_identifier}^FS\n'
          '^FO310,112^A0N,20,20^FD${subject_identifier} (${initials})^FS\n'
          '^FO310,132^A0N,20,20^FDDOB: ${dob} ${gender}^FS\n'
          '^FO310,152^A0N,25,20^FD${drawn_datetime}^FS\n'
