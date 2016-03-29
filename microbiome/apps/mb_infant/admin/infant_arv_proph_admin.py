77a38a492d62b70abfa4d78275402397a38b72d7
Leslie <leslie.kep@gmail.com>
2016-03-29 18:15:03 +0200

    infant arv proph admin

--- microbiome/apps/mb_infant/admin/infant_arv_proph_admin.py
+++ microbiome/apps/mb_infant/admin/infant_arv_proph_admin.py
@@ -2,15 +2,26 @@ from collections import OrderedDict
 
 from django.contrib import admin
 
+from edc_base.modeladmin.admin import BaseTabularInline
 from edc_export.actions import export_as_csv_action
 
 from ..models import InfantArvProphMod, InfantArvProph
+from ..forms import InfantArvProphForm, InfantArvProphModForm
 
 from .base_infant_scheduled_modeladmin import BaseInfantScheduleModelAdmin
 
 
+class InfantArvProphModInline(BaseTabularInline):
+
+    model = InfantArvProphMod
+    form = InfantArvProphModForm
+    extra = 1
+
+
 class InfantArvProphAdmin(BaseInfantScheduleModelAdmin):
 
+    form = InfantArvProphForm
+    inlines = [InfantArvProphModInline, ]
     radio_fields = {
         'prophylatic_nvp': admin.VERTICAL,
         'arv_status': admin.VERTICAL,
@@ -29,10 +40,13 @@ class InfantArvProphAdmin(BaseInfantScheduleModelAdmin):
                  'dob': 'infant_visit__appointment__registered_subject__dob',
                  }),
         )]
+
 admin.site.register(InfantArvProph, InfantArvProphAdmin)
 
 
 class InfantArvProphModAdmin(BaseInfantScheduleModelAdmin):
+
+    form = InfantArvProphModForm
 
     list_filter = ('infant_arv_proph',)
 
